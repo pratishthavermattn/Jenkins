57dd858bb2ea2d5ac10016c68d4c3e2d4f735207
Pratishtha Verma <78463655+pratishthavermattn@users.noreply.github.com>
2021-10-03 20:31:09 +0530

    Update test_poc.py

--- test_poc.py
+++ test_poc.py
@@ -56,7 +56,7 @@ for x,y,z in zip(df['tag'], df['job_name'], df['pipeline_group']):
         print(utf8string)
         if y in utf8string:
             print("Present")
-            data = '{ "environment_variables": [ { "name": "DOCKER_TAG", "secure": false, "value": "'+x+'" } ], "materials": [ { "fingerprint": "3f0eb2f8a536c4ae3ced311c1679ea0a6593e081c2a4d0286e28c9945ff7d8ea", "revision": "ghghghgh" } ], "update_materials_before_scheduling": true }'
+            data = '{ "environment_variables": [ { "name": "DOCKER_TAG", "secure": false, "value": "'+x+'" } ], "materials": [ { "fingerprint": "3f0eb2f8a536c4ae3ced311c1679ea0a6593e081c2a4d0286e28c9945ff7d8ea", "revision": "da2fc65f42e857ee969a68a6a6d7adef66644326" } ], "update_materials_before_scheduling": true }'
             response = requests.post('http://localhost:8153/go/api/pipelines/'+y+'/schedule', headers=headers, data=data)
         else:
             print("Not present")
