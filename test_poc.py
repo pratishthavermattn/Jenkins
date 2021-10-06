ed5cdb8ef5a48f96a064685740d3057994c56811
Pratishtha Verma <78463655+pratishthavermattn@users.noreply.github.com>
2021-10-06 18:19:27 +0530

    Integrated

--- test_poc.py
+++ test_poc.py
@@ -32,9 +32,12 @@ headers = {
     'Content-Type': 'application/json',
 }
 
-df = pd.read_csv('gocd.csv')
+df = pd.read_csv('go.csv')
 df['job_name'] = df.app_name + '-' + df.env_name
-df.to_csv("gocd.csv", index=False)
+df.to_csv("go.csv", index=False)
+
+materials=[]
+update = 'true'
 
 #history
 response_history = requests.get('http://localhost:8153/go/api/pipelines/api-service-config-prod/history', headers=headers)
@@ -46,16 +49,19 @@ resp_history = response_history.json()
 var1 = resp_history['pipelines'][0]['build_cause']['material_revisions'][0]['modifications'][0]['revision']
 var2 = resp_history['pipelines'][1]['build_cause']['material_revisions'][0]['modifications'][0]['revision']
 
-for x,y,z in zip(df['tag'], df['job_name'], df['pipeline_group']):
+for x,y,z,w in zip(df['tag'], df['job_name'], df['pipeline_group'], df['revision']):
     response_pipeline = requests.get(f"http://localhost:8153/go/api/admin/pipeline_groups/{z}", headers=headers)
     resp_pipeline = response_pipeline.json()
     utf8string = []
     for name in resp_pipeline['pipelines']:
         pipeline_name = name['name']
         utf8string.append(pipeline_name)
-        print(utf8string)
-        if y in utf8string:
-            data = '{ "environment_variables": [ { "name": "DOCKER_TAG", "secure": false, "value": "'+x+'" } ], "materials": [ { "fingerprint": "3f0eb2f8a536c4ae3ced311c1679ea0a6593e081c2a4d0286e28c9945ff7d8ea", "revision": "da2fc65f42e857ee969a68a6a6d7adef66644326" } ], "update_materials_before_scheduling": true }'
+        if y in utf8string and w!= "none":
+            print("Present")
+            materials = [{ "fingerprint": "3f0eb2f8a536c4ae3ced311c1679ea0a6593e081c2a4d0286e28c9945ff7d8ea", "revision": w }]
+            update = "false"
+            data = '{ "environment_variables": [ { "name": "DOCKER_TAG", "secure": false, "value": "'+x+'" } ], "materials": '+str(materials)+', "update_materials_before_scheduling": '+update+' }'
+            #[ { "fingerprint": "3f0eb2f8a536c4ae3ced311c1679ea0a6593e081c2a4d0286e28c9945ff7d8ea", "revision": "da2fc65f42e857ee969a68a6a6d7adef66644326" } ]]
             response = requests.post('http://localhost:8153/go/api/pipelines/'+y+'/schedule', headers=headers, data=data)
         else:
             print("Not present")
